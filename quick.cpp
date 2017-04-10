ae61e806c28f53e95fea3ddfeb50f0bebe6c37a0
wiktorgoral <wiktor.goral@gmail.com>
2017-04-10 18:26:43 +0200

    sortwanie 2 fukncja

--- quick.cpp
+++ quick.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 using namespace std;
 
-int partition(int tablica[], int p, int r) // dzielimy tablice na dwie czesci, w pierwszej wszystkie liczby sa mniejsze badz rowne x, w drugiej wieksze lub rowne od x
+int partition(int tablica[3], int p, int r) // dzielimy tablice na dwie czesci, w pierwszej wszystkie liczby sa mniejsze badz rowne x, w drugiej wieksze lub rowne od x
 {
-	int x = tablica[p]; // obieramy x 
+	int x = tablica[p]; // obieramy x
 	int i = p, j = r, w; // i, j - indeksy w tabeli
 	while (true) // petla nieskonczona - wychodzimy z niej tylko przez return j
 	{
-		while (tablica[j] > x) // dopoki elementy sa wieksze od x 
+		while (tablica[j] > x) // dopoki elementy sa wieksze od x
 			j--;
 		while (tablica[i] < x) // dopoki elementy sa mniejsze od x
 			i++;
@@ -27,8 +27,8 @@ int partition(int tablica[], int p, int r) // dzielimy tablice na dwie czesci, w
 void quicksort(int tablica[], int p, int r) // sortowanie szybkie
 {
 	int q;
-	if (p < r) 
-	{	
+	if (p < r)
+	{
 		q = partition(tablica,p,r); // dzielimy tablice na dwie czesci; q oznacza punkt podzialu
 		quicksort(tablica, p, q); // wywolujemy rekurencyjnie quicksort dla pierwszej czesci tablicy
 		quicksort(tablica, q+1, r); // wywolujemy rekurencyjnie quicksort dla drugiej czesci tablicy
